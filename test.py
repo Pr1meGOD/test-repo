949fb5eb94c461e0846527befc8b22decc527351
Pr1meGOD <cubingcosmos22@gmail.com>
2024-04-16 18:13:30 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,2 +1,25 @@
-import json
-import boto3
+
+try:
+    
+  import json
+  import boto3
+  print("all modelues loaded...")
+except Exception as e:
+  print ("Some modules are missing")
+
+def lambda_handler(event, context):
+
+    client_sentiment = boto3.client("comprehend")
+    Text = event.get("Text", None)
+    response = client_sentiment.detect_sentiment(
+        Text=Text,
+        LanguageCode='en'
+        )
+    
+    print(response)
+
+    return {
+        'status code': 200,
+    }
+    
+    

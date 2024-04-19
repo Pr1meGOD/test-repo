d7e9e11ffc8410d68d271a1a9f0f2dc752ef9c0b
Pr1meGOD <cubingcosmos22@gmail.com>
2024-04-19 15:55:24 +0530

    d

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello, World!")
+
+try:
+    
+  import json
+  import boto3
+  print("all modules loaded...")
+except Exception as e:
+  print ("Some modules are missing {}".format(e))
+
+def lambda_handler(event, context):
+
+    client_sentiment = boto3.client("comprehend")
+    Text = event.get("Text", None)
+    response = client_sentiment.detect_sentiment(
+        Text=Text,
+        LanguageCode='en'
+        )
+    
+    print(response)
+
+    return {
+        'status code': 200,
+        'body' : json.dumps('Hello from Lambda!'),
+        "Response": response
+    }
+    
+    

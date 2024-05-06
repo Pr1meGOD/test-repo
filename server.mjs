8cf32ab684aeff0c036375ac4aef10e9920e2f81
Pr1meGOD <cubingcosmos22@gmail.com>
2024-05-06 16:08:17 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,16 +1,17 @@
-import express from 'express';
-import fetch from 'node-fetch';
+const express = require('express');
+const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.get('/hindustantimes/news', async (req, res) => {
+// Route to fetch news from Hindustan Times and serve it to the frontend
+app.get('/api/news', async (req, res) => {
     try {
         const response = await fetch('https://www.hindustantimes.com/india-news');
         const html = await response.text();
         res.send(html);
     } catch (error) {
-        console.error('Error fetching data:', error);
-        res.status(500).send('Error fetching data');
+        console.error('Error fetching news:', error);
+        res.status(500).send('Error fetching news');
     }
 });
 

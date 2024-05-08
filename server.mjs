af8ceb3291c1cbb7c7c6b5bedb6cb7783e317fdc
Pr1meGOD <cubingcosmos22@gmail.com>
2024-05-08 17:04:01 +0530

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -7,30 +7,28 @@ const port = 3000;
 
 // Middleware to enable CORS
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*'); // Allow requests from any origin
-    res.setHeader('Access-Control-Allow-Methods', '*'); // Allow only GET requests
-    res.setHeader('Access-Control-Allow-Headers', '*'); // Allow only Content-Type header
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', '*');
+    res.setHeader('Access-Control-Allow-Headers', '*');
     next();
 });
 
 // API endpoint to fetch news headlines from BBC and perform sentiment analysis
 app.get('/news', async (req, res) => {
     try {
-        const apiKey = '8299ddae71074acd8232edcfef9b7fb8'; // Your News API key
+        const apiKey = '8299ddae71074acd8232edcfef9b7fb8';
         const response = await fetch(`https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=${apiKey}`);
         const data = await response.json();
 
         if (data.status === 'ok') {
             const articles = data.articles;
-            const newsHeadlines = articles.map(async article => {
+            const newsHeadlines = await Promise.all(articles.map(async article => {
                 const title = article.title;
                 const link = article.url;
-                const sentiment = await getSentiment(title); // Call async function to get sentiment
+                const sentiment = await getSentiment(title);
                 return { title, link, sentiment };
-            });
-            Promise.all(newsHeadlines).then(results => {
-                res.json(results);
-            });
+            }));
+            res.json(newsHeadlines);
         } else {
             throw new Error('Failed to fetch news headlines from BBC');
         }
@@ -40,19 +38,20 @@ app.get('/news', async (req, res) => {
     }
 });
 
-// Function to perform sentiment analysis (dummy function for demonstration)
+// Function to perform sentiment analysis
 async function getSentiment(title) {
     try {
-        const sentimentAPI = 'https://xhkc56io19.execute-api.us-east-1.amazonaws.com/dev'; // Sentiment analysis API endpoint
+        const sentimentAPI = 'https://xhkc56io19.execute-api.us-east-1.amazonaws.com/dev';
         const response = await axios.post(sentimentAPI, { text: title });
         const sentiment = response.data.sentiment;
         return sentiment;
     } catch (error) {
         console.error('Error fetching sentiment:', error);
-        return 'N/A'; // Return 'N/A' if sentiment analysis fails
+        return 'N/A';
     }
 }
 
 app.listen(port, () => {
     console.log(`Backend server running at http://localhost:${port}`);
 });
+

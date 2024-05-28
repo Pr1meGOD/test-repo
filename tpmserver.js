11181a95d14c647556fe5fa71a1e675c8918691a
Pr1meGOD <cubingcosmos22@gmail.com>
2024-05-28 17:28:56 +0530

    u

--- tpmserver.js
+++ tpmserver.js
@@ -22,13 +22,20 @@ import('node-fetch').then(async ({ default: fetch }) => {
 
         try {
             let apiUrl;
-            const apiKey = 'e382cf0baf104c3ca084d880a340dfa9';
+            const cnnApiKey = 'e382cf0baf104c3ca084d880a340dfa9';
+            const bbcApiKey = '8299ddae71074acd8232edcfef9b7fb8';
+            const guardianApiKey = '4c73a5e6-93d1-4051-8e96-933b8d4fa06b';
+            const nytimesApiKey = 'LMv5Elsw8GmMHGOhyr3MQTuSGHNWgxgu';
 
             // Define the API URL based on the provided website
             if (website === 'cnn-news') {
-                apiUrl = `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${apiKey}`;
+                apiUrl = `https://newsapi.org/v2/top-headlines?sources=cnn&apiKey=${cnnApiKey}`;
             } else if (website === 'bbc-news') {
-                apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${apiKey}`;
+                apiUrl = `https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=${bbcApiKey}`;
+            } else if (website === 'guardian-news') {
+                apiUrl = `https://content.guardianapis.com/search?api-key=${guardianApiKey}`;
+            } else if (website === 'nytimes-news') {
+                apiUrl = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${nytimesApiKey}`;
             } else {
                 return res.status(400).json({ error: 'Unsupported website' });
             }
@@ -36,8 +43,22 @@ import('node-fetch').then(async ({ default: fetch }) => {
             const response = await fetch(apiUrl);
             const data = await response.json();
 
-            if (data.status === 'ok') {
+            if ((website === 'cnn-news' || website === 'bbc-news') && data.status === 'ok') {
                 const articles = data.articles;
+                const newsHeadlines = articles.map(article => ({
+                    title: article.title,
+                    link: article.url
+                }));
+                res.json(newsHeadlines);
+            } else if (website === 'guardian-news' && data.response.status === 'ok') {
+                const articles = data.response.results;
+                const newsHeadlines = articles.map(article => ({
+                    title: article.webTitle,
+                    link: article.webUrl
+                }));
+                res.json(newsHeadlines);
+            } else if (website === 'nytimes-news' && data.status === 'OK') {
+                const articles = data.results;
                 const newsHeadlines = articles.map(article => ({
                     title: article.title,
                     link: article.url

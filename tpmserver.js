49dadb722460851bb49e261406e9b81b461907a5
Pr1meGOD <cubingcosmos22@gmail.com>
2024-06-07 16:12:10 +0530

    Update tpmserver.js

--- tpmserver.js
+++ tpmserver.js
@@ -55,6 +55,8 @@ app.get('/news', async (req, res) => {
             apiUrl = `https://newsdata.io/api/1/news?apikey=pub_4567897d90734c71bf518d4098e2d2345a31c&q=https://www.foxnews.com/&language=en`;
         } else if (website === 'news18-news') {
             apiUrl = `https://newsdata.io/api/1/news?apikey=pub_4553266773c7d8f5c57a2944449b3041a19b2&q=https://www.news18.com/&language=en`;
+        } else if (website === 'time-news') {
+            apiUrl = `https://newsdata.io/api/1/news?apikey=pub_4553266773c7d8f5c57a2944449b3041a19b2&q=https://time.com/`;
         } else {
             return res.status(400).json({ error: 'Unsupported website' });
         }
@@ -97,7 +99,7 @@ app.get('/news', async (req, res) => {
                 articles = data.response.results.slice(0, 10);
             } else if (website === 'nytimes-news' && data.status === 'OK') {
                 articles = data.results.slice(0, 10);
-            } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news') {
+            } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news') {
                 articles = data.results.slice(0, 10);
             } else {
                 throw new Error(`Failed to fetch news headlines from ${website}`);
@@ -109,7 +111,7 @@ app.get('/news', async (req, res) => {
                     if (website === 'guardian-news') {
                         title = article.webTitle;
                         link = article.webUrl;
-                    } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news') {
+                    } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news') {
                         title = article.title;
                         link = `${article.link}`;
                     } else {

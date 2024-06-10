d99c79d744f9030924bc69acf655df181a51ee44
Pr1meGOD <cubingcosmos22@gmail.com>
2024-06-10 12:29:01 +0530

    Update tpmserver.js

--- tpmserver.js
+++ tpmserver.js
@@ -69,6 +69,8 @@ app.get('/news', async (req, res) => {
             apiUrl = `https://newsdata.io/api/1/news?apikey=pub_4567897d90734c71bf518d4098e2d2345a31c&q=https://www.huffpost.com/`;
         } else if (website === 'dna-news') {
             apiUrl = `https://newsdata.io/api/1/news?apikey=pub_45702963c65c4021894ccf1858d05f001f0e0&q=https://www.dnaindia.com/&language=en`;
+        } else if (website === 'abp-news') {
+            apiUrl = `https://newsdata.io/api/1/news?apikey=pub_46055dd31b8f4667dc66d976f583b226b4150&q=https://news.abplive.com/ `;
         } else {
             return res.status(400).json({ error: 'Unsupported website' });
         }
@@ -111,7 +113,7 @@ app.get('/news', async (req, res) => {
                 articles = data.response.results.slice(0, 10);
             } else if (website === 'nytimes-news' && data.status === 'OK') {
                 articles = data.results.slice(0, 10);
-            } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news') {
+            } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news' || website === 'abp-news') {
                 articles = data.results.slice(0, 10);
             } else {
                 throw new Error(`Failed to fetch news headlines from ${website}`);
@@ -123,7 +125,7 @@ app.get('/news', async (req, res) => {
                     if (website === 'guardian-news') {
                         title = article.webTitle;
                         link = article.webUrl;
-                    } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news') {
+                    } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news' || website === 'abp-news') {
                         title = article.title;
                         link = `${article.link}`;
                     } else {

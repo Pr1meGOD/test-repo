1ffe308e933e492769495568425bd0a4076b9cdb
Pr1meGOD <cubingcosmos22@gmail.com>
2024-06-11 12:03:18 +0530

    Update smoltest.js

--- smoltest.js
+++ smoltest.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.get('/news', async (req, res) => {
     try {
         const apiKey = 'pub_46055dd31b8f4667dc66d976f583b226b4150';
-        const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_46055dd31b8f4667dc66d976f583b226b4150&q=https://www.tribuneindia.com/   `);
+        const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_46055dd31b8f4667dc66d976f583b226b4150&q=https://www.thequint.com/   `);
         const data = await response.json();
 
         if (data.status === 'success') {

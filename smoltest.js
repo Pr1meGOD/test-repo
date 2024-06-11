20fcdc178daa6685fdfd9e706dc6b5566329311d
Pr1meGOD <cubingcosmos22@gmail.com>
2024-06-11 16:38:57 +0530

    Update smoltest.js

--- smoltest.js
+++ smoltest.js
@@ -18,7 +18,7 @@ app.use((req, res, next) => {
 app.get('/news', async (req, res) => {
     try {
         const apiKey = 'pub_46055dd31b8f4667dc66d976f583b226b4150';
-        const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_46129c615d767bb4bf245e4fe94dba43e26a4&q=https://www.latimes.com/   `);
+        const response = await fetch(`https://newsdata.io/api/1/news?apikey=pub_46129c615d767bb4bf245e4fe94dba43e26a4&q=https://www.ctvnews.ca/ `);
         const data = await response.json();
 
         if (data.status === 'success') {

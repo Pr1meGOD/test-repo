04273d70fe1f1736a407ad9890155b81a722e8d5
Pr1meGOD <cubingcosmos22@gmail.com>
2024-06-11 11:52:32 +0530

    Update tpmserver.js

--- tpmserver.js
+++ tpmserver.js
@@ -21,7 +21,7 @@ app.get('/news', async (req, res) => {
     if (!website) {
         return res.status(400).json({ error: 'Website is required' });
     }
-    
+
     try {
         let apiUrl;
 
@@ -89,6 +89,8 @@ app.get('/news', async (req, res) => {
             apiUrl = `https://newsdata.io/api/1/news?apikey=pub_46070599d3be50dc47d16188b9c0e0c2f66ff&q=https://www.rt.com/`;
         } else if (website === 'sky-news') {
             apiUrl = `https://newsdata.io/api/1/news?apikey=pub_46070599d3be50dc47d16188b9c0e0c2f66ff&q=https://news.sky.com/ `;
+        } else if (website === 'nypost-news') {
+            apiUrl = `https://newsdata.io/api/1/news?apikey=pub_46055dd31b8f4667dc66d976f583b226b4150&q=https://nypost.com/ `;
         } else {
             return res.status(400).json({ error: 'Unsupported website' });
         }
@@ -131,7 +133,7 @@ app.get('/news', async (req, res) => {
                 articles = data.response.results.slice(0, 10);
             } else if (website === 'nytimes-news' && data.status === 'OK') {
                 articles = data.results.slice(0, 10);
-            } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news' || website === 'abp-news' || website === 'newsweek-news' || website === 'firstpost-news' || website === 'bi-news' || website === 'ign-news' || website === 'npr-news' || website === 'wsj-news' || website === 'cbs-news' || website === 'rt-news' || website === 'sky-news')  {
+            } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news' || website === 'abp-news' || website === 'newsweek-news' || website === 'firstpost-news' || website === 'bi-news' || website === 'ign-news' || website === 'npr-news' || website === 'wsj-news' || website === 'cbs-news' || website === 'rt-news' || website === 'sky-news' || website === 'nypost-news')  {
                 articles = data.results.slice(0, 10);
             } else {
                 throw new Error(`Failed to fetch news headlines from ${website}`);
@@ -143,7 +145,7 @@ app.get('/news', async (req, res) => {
                     if (website === 'guardian-news') {
                         title = article.webTitle;
                         link = article.webUrl;
-                    } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news' || website === 'abp-news' || website === 'newsweek-news' || website === 'firstpost-news' || website === 'bi-news' || website === 'ign-news' || website === 'npr-news' || website === 'wsj-news' || website === 'cbs-news' || website === 'rt-news' || website === 'sky-news') {
+                    } else if (website === 'livemint-news' || website === 'ie-news' || website === 'nbc-news' || website === 'thehindu-news' || website === 'zee-news' || website === 'wp-news' || website === 'forbes-news' || website === 'fox-news' || website === 'news18-news' || website === 'time-news' || website === 'vox-news' || website === 'indiatoday-news' || website === 'dc-news' || website === 'ndtv-news' || website === 'huffpost-news' || website === 'dna-news' || website === 'abp-news' || website === 'newsweek-news' || website === 'firstpost-news' || website === 'bi-news' || website === 'ign-news' || website === 'npr-news' || website === 'wsj-news' || website === 'cbs-news' || website === 'rt-news' || website === 'sky-news' || website === 'nypost-news') {
                         title = article.title;
                         link = `${article.link}`;
                     } else {
